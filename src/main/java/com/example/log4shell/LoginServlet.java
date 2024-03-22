909d9f48e17ac759d9d360bf1366915f97214e61
Anton Volodchenko <avolodchenko@ptsecurity.com>
2024-03-22 17:44:17 +0300

    Init

---
+++ src/main/java/com/example/log4shell/LoginServlet.java
@@ -0,0 +1,34 @@
+package com.example.log4shell;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+
+@WebServlet(name = "loginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+	
+    final Logger logger = LogManager.getLogger("Test");
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userName = req.getParameter("uname");
+        String password = req.getParameter("password");
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        if(userName.equals("admin") && password.equals("password")){
+            out.println("Welcome Back Admin");
+        }
+        else{
+            out.println("Wrong login/password for user");
+        }
+    }
+}
